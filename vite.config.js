0181e01dcdfe13912e72102baad843cbf062bb7d
EsraaHamrawy <Esraa889675@gmail.com>
2025-05-25 17:08:18 +0300

    path amd img

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Digital_Center/',
+  base: '/digital-center/',
   plugins: [react()],
   resolve: {
     alias: {
