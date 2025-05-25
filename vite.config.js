01e5426d88be7947d7615fc11470b3228dda19f6
EsraaHamrawy <Esraa889675@gmail.com>
2025-05-25 10:58:25 +0300

    deploy1

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Digital_Center/',
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
+      // eslint-disable-next-line no-undefd
       "@": path.resolve(__dirname, "./src"),
     },
   },
