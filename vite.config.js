bc096b90ffeeb39b0acd9493cbc038d7a3f9cf15
EsraaHamrawy <Esraa889675@gmail.com>
2025-05-25 18:17:45 +0300

    configvite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,31 @@
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/digital-center/',
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       // eslint-disable-next-line no-undefd
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+// // vite.config.js
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/digital-center/',
+  base: '/degetai-center/', // your GitHub repo name as base path
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undefd
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
